28ae61e3e807bfa5f0d633763f41b5c06be125c7
zyndiecate <kevin.reet@googlemail.com>
2011-12-03 22:34:03 +0100

    changed chmod and added example test

---
+++ examples/example_test.js
@@ -0,0 +1,60 @@
+
+
+var broke = require('broke')
+  , assert = require('assert');
+
+broke
+    .test('dummy test suite')
+
+    .start({
+        'init the test suite': {
+            process: {
+                method: function() {
+                    this.callback(undefined, 'data');
+                }
+            },
+            assert: {
+                'test suite initialized': function(err, data) {
+                    assert.isNull(err);
+                    assert.equal(data, 'data');
+                }
+            }
+        }
+    })
+
+    .batch({
+        'test your code': {
+            config: {
+                repeats: 5,
+                delay: 500,
+                calmDown: 500,
+                timeout: 2000
+            },
+            process: {
+                request: {method: 'GET', url: 'http://google.com', options: {port: 80}}
+            },
+            assert: {
+                statusCode: 301
+            }
+        }
+    })
+
+    .stop({
+        'tear down the test suite': {
+            process: {
+                method: function() {
+                    this.callback(undefined, 'data');
+                }
+            },
+            assert: {
+                'test suite back to normal': function(err, data) {
+                    assert.isNull(err);
+                    assert.equal(data, 'data');
+                }
+            }
+        }
+    })
+
+.run(module, require('./custom_assertions.js'));
+
+
